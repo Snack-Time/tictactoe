83aebb01be0d83ea2f929d0de5bdacd68153dac3
Snack-Time <wmcnally1226@gmail.com>
2022-12-11 01:03:19 -0500

    remove practice module and add practice factory

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-const calc = (() => {
-    const add = (x,y) => x + y;
-    const sub = (x,y) => x - y;
-    const div = (x,y) => x / y;
-    const mul = (x,y) => x * y;
-    return {add, sub, div, mul};
-})();
+// Modules //
+const gameBoard = () => {
 
-console.log(calc.add(3,4))
-console.log(calc.sub(4,3))
-console.log(calc.div(6,2))
-console.log(calc.mul(2,2))
+}
+
+const displayController = () => {
+
+}
+
+// Factories //
+
+const players = (name, xo) => {
+    console.log(`${name} is playing with ${xo}'s!`);
+    return {name, xo};
+}
+
+jeff = players('jeff', 'X');
+mark = players('mark', 'O');
+
+// Query Selectors //
