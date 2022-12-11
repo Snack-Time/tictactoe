a6eace1a5d46cd8168ced9685dd25caf07297ec7
Snack-Time <wmcnally1226@gmail.com>
2022-12-11 00:57:55 -0500

    add simple module to js file for practice

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-let add = (x,y) => {
-    sum = x + y;
-    console.log(sum);
-}
+const calc = (() => {
+    const add = (x,y) => x + y;
+    const sub = (x,y) => x - y;
+    const div = (x,y) => x / y;
+    const mul = (x,y) => x * y;
+    return {add, sub, div, mul};
+})();
+
+console.log(calc.add(3,4))
+console.log(calc.sub(4,3))
+console.log(calc.div(6,2))
+console.log(calc.mul(2,2))
